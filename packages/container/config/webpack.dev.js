86470ec9aaad7555bef927f26a09818c4780a296
wibr <winfried.brinkhuis@cm.com>
2023-10-04 16:58:03 +0200

    config updates

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,9 +1,7 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
-
 
 const devConfig = {
     mode: 'development',
@@ -20,9 +18,6 @@ const devConfig = {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
             shared: packageJson.dependencies
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
         })
     ]
 }
